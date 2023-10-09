81f8e6745dd44a9fabaa4b32250ec3082db39da7
Chae Ho Shin <gshin3@live.com>
2023-10-10 01:09:55 +0900

    fix plugin errors

--- Source/PCLTest.Target.cs
+++ Source/PCLTest.Target.cs
@@ -9,7 +9,7 @@ public class PCLTestTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
-		ExtraModuleNames.AddRange(new string[] { "PCLTest", "Open3DUE5" });
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_2;
+		ExtraModuleNames.AddRange(new string[] { "PCLTest", "Open3DUE5", "Voxel" });
 	}
 }
