0bb664a35a06f7f0a92725b9a5e9ef288a2c95e8
17신채호 <gshin3@live.com>
2023-11-26 20:53:10 +0900

    add sensor pointcloud

--- Source/PCLTest/PCLTestGameModeBase.cpp
+++ Source/PCLTest/PCLTestGameModeBase.cpp
@@ -2,9 +2,3 @@
 
 
 #include "PCLTestGameModeBase.h"
-THIRD_PARTY_INCLUDES_START
-#pragma push_macro("check")   // store 'check' macro current definition
-#undef check  // undef to avoid conflicts
-#include "open3d/Open3D.h"
-#pragma pop_macro("check")  // restore definition
-THIRD_PARTY_INCLUDES_END
